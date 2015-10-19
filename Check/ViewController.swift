fdd0f4d9ab05bdc6a986e1590cccdd24cb09a3d7
waka-ka <wakaka2277@gmail.com>
2015-10-19 22:51:22 +0900

    Test commit

---
+++ Check/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Check
+//
+//  Created by 河村和始 on 10/19/15.
+//  Copyright © 2015 Kazushi Kawamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
